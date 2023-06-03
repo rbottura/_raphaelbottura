e4b5328e36de36f100316d18b1577b29952753d3
rbottura <bottura.raphael@gmail.com>
2023-06-03 20:49:57 +0200

    add loading events

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var mediaElements = document.querySelectorAll('video, img');
+    var totalMediaElements = mediaElements.length;
+    var loadedMediaCount = 0;
 
-window.addEventListener('load', function () {
-    console.log('hey')
+    // console.log(loadedMediaCount)
+    function checkAllMediaLoaded() {
+        loadedMediaCount++;
+        if (loadedMediaCount === totalMediaElements) {
+            var body = document.body;
+            body.removeAttribute('hidden');
+        }
+    }
+
+    mediaElements.forEach(function (media) {
+        // console.log(media)
+        media.addEventListener('load', checkAllMediaLoaded);
+        media.addEventListener('error', checkAllMediaLoaded);
+        if (media.complete) {
+            checkAllMediaLoaded();
+        }
+    });
+
+    
     let random = Math.floor(Math.random() * 4) + 1
     let random_sm_vid = './videos/smart_phone_vid_(' + Math.floor(random) + ').mp4'
     // let random_sm_vid = './videos/smart_phone_vid_(1).mp4'
@@ -27,9 +48,9 @@ let loop = 0;
 let loopSprites = true
 function animateSprites() {
     let imgs = document.querySelectorAll('.sprites')
-    let posterIndex = Math.floor(indexSprite/3)
-    
-    if(posterIndex == 3) {
+    let posterIndex = Math.floor(indexSprite / 3)
+
+    if (posterIndex == 3) {
         posterIndex = 0;
     }
 
@@ -40,10 +61,10 @@ function animateSprites() {
     } else {
         color = 'transparent'
     }
-    
+
     imgs[indexSprite].style.backgroundColor = color;
     imgs[indexSprite].style.transform = 'rotate(' + 90 * loop + 'deg)'
-    
+
     let posters1 = document.querySelectorAll('.gp_posters_1')
     if (indexSprite % 3 == 0) {
         for (let i = 0; i < posters1.length; i++) {
@@ -51,7 +72,7 @@ function animateSprites() {
         }
         posters1[posterIndex].style.flexGrow = '1.2'
     }
-    
+
     indexSprite++
     if (indexSprite == imgs.length) {
         indexSprite = 0
